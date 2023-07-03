f9bb43ddaceb5c9695992e1bd954dc5375273c15
Steven Conaway <steven.conaway@icloud.com>
2023-07-02 20:45:26 -0400

    header and app!

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 import './style.css';
+
+// handle header_[date, greet]
+document.getElementById('header_date').innerHTML =
+    new Date().toLocaleDateString('en-US', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+    });
+
+let currentTime = new Date();
+let greet = Math.floor(currentTime.getHours() / 6);
+switch (greet) {
+    case 0:
+        document.getElementById('header_greet').innerHTML = 'Good night!';
+        break;
+    case 1:
+        document.getElementById('header_greet').innerHTML = 'Good morning!';
+        break;
+    case 2:
+        document.getElementById('header_greet').innerHTML = 'Good afternoon!';
+        break;
+    case 3:
+        document.getElementById('header_greet').innerHTML = 'Good evening!';
+        break;
+}
