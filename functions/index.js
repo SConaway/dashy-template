d643427669bdfcbdd679913f1eb85de07a4e743f
Steven Conaway <steven.conaway@icloud.com>
2023-07-14 16:20:06 -0400

    send errors too

--- functions/index.js
+++ functions/index.js
@@ -2,15 +2,20 @@ export async function onRequest(context) {
     // return new Response(
     //     (await fetch(context.request.url + '/index.html')).body,
     // );
-    const f = await fetch(context.request.url + '/index.html');
+    try {
+        const f = await fetch(context.request.url + '/index.html');
 
-    if (f.status === 200) {
-        const body = await f.text();
+        if (f.status === 200) {
+            const body = await f.text();
 
-        return new Response(body, {
-            headers: {
-                'Content-Type': 'text/html',
-            },
-        });
+            return new Response(body, {
+                headers: {
+                    'Content-Type': 'text/html',
+                },
+            });
+        }
+    } catch (e) {
+        console.error(e);
+        return new Response(e.message);
     }
 }
