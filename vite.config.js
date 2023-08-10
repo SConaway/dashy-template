3634b8ea957fa9df1cfe7299ba72776b81e9e659
Steven Conaway <steven.conaway@icloud.com>
2023-08-09 17:19:59 -0700

    initial loading

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+import handlebars from 'vite-plugin-handlebars';
+
 /** @type {import('vite').UserConfig} */
 export default {
     build: {
@@ -6,4 +8,56 @@ export default {
         sourcemap: true,
         minify: false,
     },
+    plugins: [
+        handlebars({
+            async context() {
+                return {
+                    apps: [
+                        {
+                            name: 'GitHub',
+                            url: 'github.com/sconaway',
+                            icon: 'arcticons:github',
+                        },
+                        {
+                            name: 'Lobsters',
+                            url: 'lobste.rs',
+                            icon: 'fluent-emoji-high-contrast:lobster',
+                        },
+                    ],
+                    links: [
+                        {
+                            category: 'Tech',
+                            links: [
+                                {
+                                    name: 'github',
+                                    url: 'github.com/sconaway',
+                                },
+                                {
+                                    name: 'Lobsters',
+                                    url: 'lobste.rs',
+                                },
+                            ],
+                        },
+                        {
+                            category: 'Tech',
+                            links: [
+                                {
+                                    name: 'github',
+                                    url: 'github.com/sconaway',
+                                },
+                                {
+                                    name: 'Lobsters',
+                                    url: 'lobste.rs',
+                                },
+                            ],
+                        },
+                    ],
+                };
+            },
+            // helpers: {
+            //     loog: (value) =>
+            //         console.log(JSON.stringify(value, null, 4)) || value,
+            // },
+        }),
+    ],
 };
