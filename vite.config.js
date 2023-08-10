4c32a1d84b3533c57f7d3344e050c2ca48774589
Steven Conaway <steven.conaway@icloud.com>
2023-08-09 17:36:30 -0700

    load the data from json

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import handlebars from 'vite-plugin-handlebars';
+import fsp from 'fs/promises';
 
 /** @type {import('vite').UserConfig} */
 export default {
@@ -11,48 +12,19 @@ export default {
     plugins: [
         handlebars({
             async context() {
-                return {
-                    apps: [
-                        {
-                            name: 'GitHub',
-                            url: 'github.com/sconaway',
-                            icon: 'arcticons:github',
-                        },
-                        {
-                            name: 'Lobsters',
-                            url: 'lobste.rs',
-                            icon: 'fluent-emoji-high-contrast:lobster',
-                        },
-                    ],
-                    links: [
-                        {
-                            category: 'Tech',
-                            links: [
-                                {
-                                    name: 'github',
-                                    url: 'github.com/sconaway',
-                                },
-                                {
-                                    name: 'Lobsters',
-                                    url: 'lobste.rs',
-                                },
-                            ],
-                        },
-                        {
-                            category: 'Tech',
-                            links: [
-                                {
-                                    name: 'github',
-                                    url: 'github.com/sconaway',
-                                },
-                                {
-                                    name: 'Lobsters',
-                                    url: 'lobste.rs',
-                                },
-                            ],
-                        },
-                    ],
-                };
+                // read data.json
+                // rethrow to describe the error
+                try {
+                    const data = await fsp
+                        .readFile('./data.json', 'utf-8')
+                        .then(JSON.parse);
+
+                    console.log(data);
+                    return data;
+                } catch (err) {
+                    console.error('[Error] Error parsing config file');
+                    throw err;
+                }
             },
             // helpers: {
             //     loog: (value) =>
